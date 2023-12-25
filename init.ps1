1fa33800048a15dd9d7304d538613f2503244861
DavidTelenko <davidtelenko11@gmail.com>
2023-12-25 11:55:58 +0200

    init script removed .wintermrc

--- init.ps1
+++ init.ps1
@@ -1,5 +1,4 @@
 $curr = pwd
 ni -i SymbolicLink -fo $profile -ta $curr/.pwshrc
-ni -i SymbolicLink -fo $env:localappdata/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json -ta $curr/.wintermrc
 rm -r -fo $env:localappdata/nvim
 ni -i Junction $env:localappdata/nvim -ta $curr/.nvim/
