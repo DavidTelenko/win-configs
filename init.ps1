79e2a4b7d858ca363b21c91ae0ae3d7ea93b244f
DavidTelenko <davidtelenko11@gmail.com>
2024-01-03 12:26:07 +0200

    nushell

--- init.ps1
+++ init.ps1
@@ -1,4 +1,9 @@
 $curr = pwd
+
 ni -i SymbolicLink -fo $profile -ta $curr/.pwshrc
+
 rm -r -fo $env:localappdata/nvim
 ni -i Junction $env:localappdata/nvim -ta $curr/.nvim/
+
+rm -r -fo $env:appdata/nushell
+ni -i Junction $env:appdata/nushell/ -ta $curr/.nushell/
