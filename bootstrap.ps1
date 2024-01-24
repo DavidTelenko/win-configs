116b4e314c07ea205ed67a305684454805b1b039
DavidTelenko <davidtelenko11@gmail.com>
2024-01-24 16:30:48 +0200

    bootstrap improved

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,62 +3,13 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 
-$buckets = @(
-    "extras",
-    "games",
-    "versions",
-)
+# copy all initialize all configs
+git clone https://github.com/DavidTelenko/win-configs.git ~/.configs
+& ~/.configs/init.ps1
 
-$packages = @(
-    "7zip",
-    "aimp",
-    "ascii-image-converter",
-    "autohotkey",
-    "clangd",
-    "cmake",
-    "coreutils",
-    "dependencies",
-    "eza",
-    "ffmpeg",
-    "fzf",
-    "gcc",
-    "git",
-    "heroic-games-launcher",
-    "innounp",
-    "make",
-    "mingw-winlibs-llvm",
-    "mpc-hc-fork",
-    "neovim",
-    "ninja",
-    "nodejs",
-    "nu",
-    "obs-studio",
-    "obsidian",
-    "oh-my-posh",
-    "paint.net",
-    "premake",
-    "pueue",
-    "python",
-    "qbittorrent",
-    "ripgrep",
-    "steam",
-    "telegram",
-    "windows-terminal",
-    "winfetch",
-    "wiztree",
-    "zig",
-    "zoxide",
-)
+scoop import ~/.configs/.scoopfile.json
 
-foreach ($bucket in $buckets) {
-    scoop buckets add $bucket
-}
-
-foreach ($package in $packages) {
-    scoop install $package
-}
-
-# separate spicetify install
+# separate spicetify+spotx install
 iex "& { $(iwr -useb 'https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1') } -confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -block_update_on -start_spoti -new_theme -adsections_off -lyrics_stat spotify"
 iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex
 iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex
