b82fe6d85fac8b92caa6f027ec4c7488894d249f
DavidTelenko <davidtelenko11@gmail.com>
2024-02-05 10:34:44 +0200

    Mon, 5 Feb 2024 10:34:44 +0200 (now)

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,13 +1,13 @@
 # Simple scoop based bootstrap script for a fresh system
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
+iex "& { $(irm get.scoop.sh) } -RunAsAdmin"
+
+scoop import ~/.configs/.scoopfile.json
 
 # copy and initialize all configs
 git clone https://github.com/DavidTelenko/win-configs.git ~/.configs
 & ~/.configs/init.ps1
-
-scoop import ~/.configs/.scoopfile.json
 
 # separate spicetify+spotx install
 iex "& { $(iwr -useb 'https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1') } -confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -block_update_on -start_spoti -new_theme -adsections_off -lyrics_stat spotify"
--- init.ps1
+++ init.ps1
@@ -1,6 +1,6 @@
 $curr = pwd
 
-ni -i SymbolicLink -fo $profile -ta $curr/.pwshrc
+ni -i SymbolicLink -fo $profile -ta $curr/.pwshrc.ps1
 ni -i SymbolicLink -fo $env:appdata/alacritty/alacritty.toml -ta $curr/.alacritty.toml
 
 rm -r -fo $env:localappdata/nvim
@@ -8,3 +8,5 @@ ni -i Junction $env:localappdata/nvim -ta $curr/.nvim/
 
 rm -r -fo $env:appdata/nushell
 ni -i Junction $env:appdata/nushell/ -ta $curr/.nushell/
+
+cat $curr/.winterm.json > ~/scoop/persist/windows-terminal/settings/settings.json
