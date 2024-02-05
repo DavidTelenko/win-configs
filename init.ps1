fa39e61694b4c70a756a031725cbe7f35781ae2b
DavidTelenko <davidtelenko11@gmail.com>
2024-02-05 08:55:40 +0200

    Mon, 5 Feb 2024 08:55:40 +0200 (now)

--- init.ps1
+++ init.ps1
@@ -1,6 +1,7 @@
 $curr = pwd
 
 ni -i SymbolicLink -fo $profile -ta $curr/.pwshrc
+ni -i SymbolicLink -fo $env:appdata/alacritty/alacritty.toml -ta $curr/.alacritty.toml
 
 rm -r -fo $env:localappdata/nvim
 ni -i Junction $env:localappdata/nvim -ta $curr/.nvim/
