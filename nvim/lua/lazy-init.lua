8a078c282e544efb49cd1433bac0547080d62f57
DavidTelenko <davidtelenko11@gmail.com>
2024-04-15 21:00:58 +0300

    icons

--- nvim/lua/lazy-init.lua
+++ nvim/lua/lazy-init.lua
@@ -2,7 +2,7 @@
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
 
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -17,12 +17,34 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  spec = "plugins",
-  change_detection = {
-    notify = false,
+require('lazy').setup {
+  root = vim.fn.stdpath 'data' .. '/lazy',
+  defaults = {
+    lazy = false,
+    version = nil,
+    cond = nil,
+  },
+  spec = 'plugins',
+  lockfile = vim.fn.stdpath 'config' .. '/lazy-lock.json',
+  concurrency = jit.os:find 'Windows' and (vim.uv.available_parallelism() * 2) or nil,
+  git = {
+    log = { '-8' },
+    timeout = 120,
+    url_format = 'https://github.com/%s.git',
+    filter = true,
+  },
+  install = {
+    missing = true,
+    colorscheme = { 'habamax' },
   },
   ui = {
+    size = { width = 0.8, height = 0.8 },
+    wrap = true,
+    border = 'rounded',
+    backdrop = 60,
+    title = 'Lazy',
+    title_pos = 'center',
+    pills = true,
     icons = {
       cmd = '⌘',
       config = '🛠',
@@ -37,8 +59,86 @@ require("lazy").setup({
       start = '🚀',
       task = '📌',
       lazy = '💤 ',
-    }
-  }
-})
+      import = ' ',
+      loaded = '',
+      not_loaded = '',
+      list = {
+        '●',
+        '➜',
+        '★',
+        '‒',
+      },
+    },
+    browser = nil,
+    throttle = 20,
+    custom_keys = {
+      ['<localleader>l'] = {
+        function(plugin)
+          require('lazy.util').float_term({ 'lazygit', 'log' }, {
+            cwd = plugin.dir,
+          })
+        end,
+        desc = 'Open lazygit log',
+      },
+
+      ['<localleader>t'] = {
+        function(plugin)
+          require('lazy.util').float_term(nil, {
+            cwd = plugin.dir,
+          })
+        end,
+        desc = 'Open terminal in plugin dir',
+      },
+    },
+  },
+  diff = {
+    cmd = 'git',
+  },
+  checker = {
+    enabled = false,
+    concurrency = nil, ---@type number? set to 1 to check for updates very slowly
+    notify = true, -- get a notification when new updates are found
+    frequency = 3600, -- check for updates every hour
+    check_pinned = false, -- check for pinned packages that can't be updated
+  },
+  change_detection = {
+    enabled = true,
+    notify = false, -- get a notification when changes are found
+  },
+  performance = {
+    cache = {
+      enabled = true,
+    },
+    reset_packpath = true, -- reset the package path to improve startup time
+    rtp = {
+      reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
+      paths = {}, -- add any custom paths here that you want to includes in the rtp
+      disabled_plugins = {
+        -- "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        -- "tarPlugin",
+        -- "tohtml",
+        -- "tutor",
+        -- "zipPlugin",
+      },
+    },
+  },
+  readme = {
+    enabled = true,
+    root = vim.fn.stdpath 'state' .. '/lazy/readme',
+    files = { 'README.md', 'lua/**/README.md' },
+    skip_if_doc_exists = true,
+  },
+  state = vim.fn.stdpath 'state' .. '/lazy/state.json', -- state info for checker and other things
+  build = {
+    warn_on_override = true,
+  },
+  profiling = {
+    loader = false,
+    require = false,
+  },
+}
 
 -- vim: ts=2 sts=2 sw=2 et
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,15 +2,53 @@
 
 return {
   'nvim-lualine/lualine.nvim',
-  -- See `:help lualine.txt`
-  opts = {
-    options = {
-      icons_enabled = false,
-      theme = require("plugins.theme").theme,
-      component_separators = '|',
-      section_separators = '',
-    },
-  },
+
+  config = function()
+    require('lualine').setup {
+      options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = '',
+        section_separators = { right = '', left = '' },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        },
+      },
+      sections = {
+        lualine_a = { { 'mode' } },
+        lualine_b = {
+          { 'branch', icon = { '󰘬', color = { fg = '#fe8019' } } },
+          { 'diff', symbols = { added = ' ', modified = ' ', removed = ' ' } },
+          'diagnostics',
+        },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { { 'location' } },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {},
+    }
+  end,
 }
 
 -- vim: ts=2 sts=2 sw=2 et
