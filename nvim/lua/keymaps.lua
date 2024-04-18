cef3c00d22b74bb8dce43805641407b8d3742e27
DavidTelenko <davidtelenko11@gmail.com>
2024-04-18 14:10:06 +0300

    netrw further enhanced

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -42,4 +42,20 @@ vim.keymap.set('t', '<C-H>', '<C-w>', { desc = 'Ctrl + Backspace "default" behav
 
 vim.keymap.set('n', '<leader>T', vim.cmd.terminal, { desc = 'Open [T]erminal' })
 
+vim.keymap.set('n', '<tab>', ':E<cr>', { desc = 'Open Explorer' })
+
+-- netrw keymaps for more 'lettered' experience
+vim.api.nvim_create_autocmd('BufModifiedSet', {
+  group = vim.api.nvim_create_augroup("netrw", { clear = false }),
+  pattern = '*',
+  callback = function()
+    if not (vim.bo and vim.bo.filetype == 'netrw') then
+      return
+    end
+    vim.keymap.set('n', 'l', '<cr>', { buffer = true, remap = true })
+    vim.keymap.set('n', 'h', '-', { buffer = true, remap = true })
+    vim.keymap.set('n', 'a', '%', { buffer = true, remap = true })
+  end
+})
+
 -- vim: ts=2 sts=2 sw=2 et
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -56,7 +56,7 @@ return {
       height = { min = 4, max = 25 }, -- min and max height of the columns
       width = { min = 20, max = 50 }, -- min and max width of the columns
       spacing = 3,                    -- spacing between columns
-      align = "center",               -- align columns left, center or right
+      align = "left",                 -- align columns left, center or right
     },
     ignore_missing = false,           -- enable this to hide mappings for which you didn't specify a label
     hidden = {
