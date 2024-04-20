1d0a3817800073ec192d22d13e403e640045ea63
DavidTelenko <davidtelenko11@gmail.com>
2024-04-20 05:26:39 +0300

    performance

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -1,6 +1,8 @@
 return {
   'mbbill/undotree',
-  keys = {},
+  keys = {
+    { '<leader>h' },
+  },
   config = function()
     vim.keymap.set('n', '<leader>h', vim.cmd.UndotreeToggle, {
       desc = 'Toggle [H]istory tree',
