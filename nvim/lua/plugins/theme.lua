6c1d9c74d185cf55e57f212bf93dc971c336f2da
DavidTelenko <davidtelenko11@gmail.com>
2024-04-22 11:01:25 +0300

    Mon, 22 Apr 2024 11:01:25 +0300 (now)

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -2,57 +2,6 @@ local transparent = true
 
 return {
   {
-    'nvim-lualine/lualine.nvim',
-    event = 'VeryLazy',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('lualine').setup {
-        options = {
-          icons_enabled = true,
-          theme = 'auto',
-          component_separators = '',
-          section_separators = { right = '', left = '' },
-          disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-          },
-          ignore_focus = {},
-          always_divide_middle = true,
-          globalstatus = false,
-          refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-          },
-        },
-        sections = {
-          lualine_a = { { 'mode' } },
-          lualine_b = {
-            { 'branch', icon = '󰘬' },
-            { 'diff', symbols = { added = ' ', modified = ' ', removed = ' ' } },
-            'diagnostics',
-          },
-          lualine_c = { 'filename' },
-          lualine_x = { 'encoding', 'filetype' },
-          lualine_y = { 'progress' },
-          lualine_z = { { 'location' } },
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { 'filename' },
-          lualine_x = { 'location' },
-          lualine_y = {},
-          lualine_z = {},
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {},
-      }
-    end,
-  },
-  {
     'lukas-reineke/indent-blankline.nvim',
     event = 'BufReadPre',
     main = 'ibl',
@@ -91,7 +40,7 @@ return {
         invert_tabline = false,
         invert_intend_guides = false,
         inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = '', -- can be "hard", "soft" or empty string
+        contrast = '',  -- can be "hard", "soft" or empty string
         palette_overrides = {},
         overrides = {},
         dim_inactive = false,
@@ -106,14 +55,14 @@ return {
     config = function()
       require('onedark').setup {
         -- Main options --
-        style = 'warm', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-        transparent = transparent, -- Show/hide background
-        term_colors = true, -- Change terminal color as per the selected theme style
-        ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+        style = 'warm',               -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+        transparent = transparent,    -- Show/hide background
+        term_colors = true,           -- Change terminal color as per the selected theme style
+        ending_tildes = false,        -- Show the end-of-buffer tildes. By default they are hidden
         cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
         -- toggle theme style ---
-        toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+        toggle_style_key = nil,                                                              -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
         toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
 
         -- Change code style ---
@@ -133,13 +82,13 @@ return {
         },
 
         -- Custom Highlights --
-        colors = {}, -- Override default colors
+        colors = {},     -- Override default colors
         highlights = {}, -- Override highlight groups
 
         -- Plugins Config --
         diagnostics = {
-          darker = true, -- darker colors for diagnostic
-          undercurl = true, -- use undercurl instead of underline for diagnostics
+          darker = true,     -- darker colors for diagnostic
+          undercurl = true,  -- use undercurl instead of underline for diagnostics
           background = true, -- use background color for virtual text
         },
       }
@@ -153,9 +102,9 @@ return {
       require('tokyonight').setup {
         -- your configuration comes here
         -- or leave it empty to use the default settings
-        style = 'storm', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        style = 'storm',           -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
         transparent = transparent, -- Enable this to disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        terminal_colors = true,    -- Configure the colors used when opening a `:terminal` in Neovim
         styles = {
           -- Style to be applied to different syntax groups
           -- Value is any valid attr-list value for `:help nvim_set_hl`
@@ -163,7 +112,7 @@ return {
           keywords = { italic = false },
           -- Background styles. Can be "dark", "transparent" or "normal"
           sidebars = 'dark', -- style for sidebars, see below
-          floats = 'dark', -- style for floating windows
+          floats = 'dark',   -- style for floating windows
         },
       }
       vim.cmd.colorscheme 'tokyonight'
