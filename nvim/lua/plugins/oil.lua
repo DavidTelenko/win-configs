5614312a90d034efe5855f0397339553d5a09aa3
DavidTelenko <davidtelenko11@gmail.com>
2024-05-15 15:00:18 +0300

    Wed, 15 May 2024 15:00:18 +0300 (now)

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -61,14 +61,14 @@ return {
       -- See :help oil-actions for a list of all available actions
       keymaps = {
         ["g?"] = "actions.show_help",
-        ["<CR>"] = "actions.select",
+        ["<right>"] = "actions.select",
         ["<C-s>"] = "actions.select_vsplit",
         ["<C-h>"] = "actions.select_split",
         ["<C-t>"] = "actions.select_tab",
         ["<C-p>"] = "actions.preview",
         ["<C-c>"] = "actions.close",
         ["<C-l>"] = "actions.refresh",
-        ["-"] = "actions.parent",
+        ["<left>"] = "actions.parent",
         ["_"] = "actions.open_cwd",
         ["`"] = "actions.cd",
         ["~"] = "actions.tcd",
@@ -81,7 +81,7 @@ return {
       use_default_keymaps = true,
       view_options = {
         -- Show files and directories that start with "."
-        show_hidden = false,
+        show_hidden = true,
         -- This function defines what is considered a "hidden" file
         is_hidden_file = function(name, bufnr)
           return vim.startswith(name, ".")
