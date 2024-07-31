921a0ca42cd2669e13c811965c63c3bfe0857b65
DavidTelenko <davidtelenko11@gmail.com>
2024-08-01 00:55:09 +0300

    Thu, 1 Aug 2024 00:55:09 +0300 (now)

--- nvim/lua/plugins/flutter-tools.lua
+++ nvim/lua/plugins/flutter-tools.lua
@@ -7,6 +7,8 @@ return {
     'stevearc/dressing.nvim',
   },
   config = function()
+    local telescope = require 'telescope.builtin'
+
     require('flutter-tools').setup {
       ui = {
         border = 'rounded',
@@ -36,7 +38,7 @@ return {
       root_patterns = { '.git', 'pubspec.yaml' }, -- patterns to find the root of your flutter project
       fvm = true, -- takes priority over path, uses <workspace>/.fvm/flutter_sdk if enabled
       widget_guides = {
-        enabled = false,
+        enabled = true,
       },
       closing_tags = {
         highlight = 'Comment', -- highlight for the closing tag
@@ -65,7 +67,42 @@ return {
           virtual_text = true, -- show the highlight using virtual text
           virtual_text_str = '■', -- the virtual text character to highlight
         },
-        on_attach = function() end,
+        on_attach = function(client, bufnr)
+          local nmap = function(keys, func, desc)
+            if desc then
+              desc = 'LSP: ' .. desc
+            end
+
+            vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+          end
+
+          nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+          nmap('gd', telescope.lsp_definitions, '[G]oto [D]efinition')
+          nmap('gr', telescope.lsp_references, '[G]oto [R]eferences')
+          nmap('gI', telescope.lsp_implementations, '[G]oto [I]mplementation')
+          nmap('<leader>D', telescope.lsp_type_definitions, 'Type [D]efinition')
+          nmap('<leader>ss', telescope.lsp_document_symbols, '[D]ocument [S]ymbols')
+          nmap('<leader>ws', telescope.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+          -- See `:help K` for why this keymap
+          nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+          nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+          -- Lesser used LSP functionality
+          nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+          nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+          nmap('<leader>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, '[W]orkspace [L]ist Folders')
+
+          -- Create a command `:Format` local to the LSP buffer
+          vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+            vim.lsp.buf.format()
+          end, { desc = 'Format current buffer with LSP' })
+        end,
         capabilities = function(config)
           return config
         end,
