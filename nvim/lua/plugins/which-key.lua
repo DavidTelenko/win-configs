427755ddd6ed7a08d11439d08d858a250bebc2e5
DavidTelenko <davidtelenko11@gmail.com>
2024-08-08 11:00:48 +0300

    Thu, 8 Aug 2024 11:00:48 +0300 (now)

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -103,7 +103,7 @@ return {
     -- document existing key chains
     require('which-key').register {
       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+      ['<leader>d'] = { name = '[D]ebug', _ = 'which_key_ignore' },
       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]efactor', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
