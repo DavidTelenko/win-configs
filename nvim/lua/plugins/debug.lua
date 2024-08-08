a5e85dec754650f25910472ee4f951b9f840c993
DavidTelenko <davidtelenko11@gmail.com>
2024-08-08 11:00:48 +0300

    Thu, 8 Aug 2024 11:00:48 +0300 (now)

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -74,15 +74,14 @@ return {
       },
     }
 
-    -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>B', function()
+    vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'Step [I]nto' })
+    vim.keymap.set('n', '<leader>do', dap.step_over, { desc = 'Step [O]ver' })
+    vim.keymap.set('n', '<leader>dO', dap.step_out, { desc = 'Step [O]ut' })
+    vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'Start/[C]ontinue' })
+    vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Toggle [B]reakpoint' })
+    vim.keymap.set('n', '<leader>dB', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-    end, { desc = 'Debug: Set Breakpoint' })
+    end, { desc = 'Set [B]reakpoint' })
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
