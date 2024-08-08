fe04fc21acf7d866784562c684483abd5e754925
DavidTelenko <davidtelenko11@gmail.com>
2024-08-09 01:42:08 +0300

    Fri, 9 Aug 2024 01:42:08 +0300 (now)

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,45 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  lazy = false,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+  },
+  events = { 'VeryLazy' },
+  config = function()
+    local harpoon = require 'harpoon'
+
+    harpoon:setup()
+
+    require('which-key').register({ ['<leader>H'] = '[H]arpoon' }, {
+      mode = { 'n' },
+    })
+
+    for i = 1, 5 do
+      vim.keymap.set('n', '<leader>' .. i, function()
+        harpoon:list():select(i)
+      end, { desc = 'Navigate to harpoon file [' .. i .. ']' })
+    end
+
+    vim.keymap.set('n', '<leader>Ha', function()
+      harpoon:list():add()
+    end, { desc = '[A]dd file to harpoon' })
+
+    vim.keymap.set('n', '<leader>Hr', function()
+      harpoon:list():remove()
+    end, { desc = '[R]remove file from harpoon' })
+
+    vim.keymap.set('n', '<leader>Hm', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'Open quick [M]enu' })
+
+    vim.keymap.set('n', '<leader>p', function()
+      harpoon:list():prev()
+    end, { desc = '[N]ext harpoon entry' })
+
+    vim.keymap.set('n', '<leader>n', function()
+      harpoon:list():next()
+    end, { desc = '[P]revious harpoon entry' })
+  end,
+}
