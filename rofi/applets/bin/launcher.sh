6619f819a5e067ee0ffe80a4c8995814429c3ce7
DavidTelenko <davidtelenko11@gmail.com>
2024-08-18 02:42:31 +0300

    rofi cleaning

--- rofi/applets/bin/launcher.sh
+++
@@ -1,7 +0,0 @@
-dir="$HOME/.config/rofi/launchers/type-2"
-theme='style-2'
-
-## Run
-rofi \
-    -show drun \
-    -theme ${dir}/${theme}.rasi
---
+++ rofi/bin/launcher.sh
@@ -0,0 +1,7 @@
+dir="$HOME/.config/rofi/themes/launchers/type-2"
+theme='style-1'
+
+## Run
+rofi \
+    -show drun \
+    -theme ${dir}/${theme}.rasi
