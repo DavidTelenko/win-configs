720d3ed3b825f4c87f128f1d8d9b2df1a4f3adc2
DavidTelenko <davidtelenko11@gmail.com>
2024-08-26 12:37:26 +0300

    player source control

---
+++ hypr/scripts/player_manager.sh
@@ -0,0 +1,29 @@
+state_file="$HOME/.config/hypr/scripts/data/player_state"
+
+select_source() {
+  readarray -t players < <(playerctl -l)
+  echo $(($1 % ${#players[@]})) > $state_file
+}
+
+next() {
+  select_source $(($(cat $state_file) + 1))
+}
+
+prev() {
+  select_source $(($(cat $state_file) - 1))
+}
+
+get() {
+  local index=$(cat $state_file)
+
+  readarray -t players < <(playerctl -l)
+
+  if ((index >= ${#players[@]})); then
+    echo 0
+	return
+  fi
+
+  echo ${players[$index]}
+}
+
+f_call=$1; shift; $f_call "$@"
--- waybar/scripts/media-info.sh
+++ waybar/scripts/media-info.sh
@@ -1,11 +1,13 @@
+player=$(~/.config/hypr/scripts/player_manager.sh get)
+
 full_metadata_fmt="{{ title }}\n{{ artist }}\n{{ album }}"
 progress_fmt="{{ duration(position) }} / {{ duration(mpris:length) }}"
 song_info_fmt="{{ artist }} - {{ title }}"
 
-metadata=$(playerctl metadata -f "$progress_fmt $song_info_fmt")
-full_metadata=$(playerctl metadata -f "$full_metadata_fmt")
+metadata=$(playerctl -p $player metadata -f "$progress_fmt $song_info_fmt")
+full_metadata=$(playerctl -p $player metadata -f "$full_metadata_fmt")
 
-status_upper="$(playerctl status)"
+status_upper="$(playerctl -p $player status)"
 status="${status_upper,,}"
 
 metadata_esc=$(echo "$metadata" | sed 's/"/\\"/g')
