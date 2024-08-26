4a2525226e62fce9625b0bf246d85815a29c93a4
DavidTelenko <davidtelenko11@gmail.com>
2024-08-26 19:41:22 +0300

    Mon, 26 Aug 2024 19:41:22 +0300 (now)

--- waybar/scripts/media-info.sh
+++ waybar/scripts/media-info.sh
@@ -1,16 +1,27 @@
-player=$(~/.config/hypr/scripts/player_manager.sh get)
+get() {
+  player=$(~/.config/hypr/scripts/player_manager.sh get)
 
-full_metadata_fmt="{{ title }}\n{{ artist }}\n{{ album }}"
-progress_fmt="{{ duration(position) }} / {{ duration(mpris:length) }}"
-song_info_fmt="{{ artist }} - {{ title }}"
+  full_metadata_fmt="{{ title }}\n{{ artist }}\n{{ album }}"
+  progress_fmt="{{ duration(position) }} / {{ duration(mpris:length) }}"
+  song_info_fmt="{{ artist }} - {{ title }}"
 
-metadata=$(playerctl -p $player metadata -f "$progress_fmt $song_info_fmt")
-full_metadata=$(playerctl -p $player metadata -f "$full_metadata_fmt")
+  metadata=$(playerctl -p $player metadata -f "$progress_fmt $song_info_fmt")
+  full_metadata=$(playerctl -p $player metadata -f "$full_metadata_fmt")
 
-status_upper="$(playerctl -p $player status)"
-status="${status_upper,,}"
+  status_upper="$(playerctl -p $player status)"
+  status="${status_upper,,}"
 
-metadata_esc=$(echo "$metadata" | sed 's/"/\\"/g')
-full_metadata_esc=$(echo "$full_metadata" | sed 's/"/\\"/g')
+  metadata_esc=$(echo "$metadata" | sed 's/"/\\"/g')
+  full_metadata_esc=$(echo "$full_metadata" | sed 's/"/\\"/g')
 
-echo {\"text\": \""$metadata_esc"\", \"alt\": \""$status"\", \"tooltip\": "\"$full_metadata_esc\"", "\"class"\": "\"$status"\"}
+  echo {\"text\": \""$metadata_esc"\", \"alt\": \""$status"\", \"tooltip\": "\"$full_metadata_esc\"", "\"class"\": "\"$status"\"}
+}
+
+can() {
+  if [ $(playerctl status) == Stopped ]; then
+    return 1
+  fi
+  return 0
+}
+
+f_call=$1; shift; $f_call "$@"
