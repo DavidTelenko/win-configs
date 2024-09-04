38062792d68fb8930a44d36cb01d963832206b49
Davyd Telenko <davyd.telenko@dataart.com>
2024-09-04 10:17:25 +0300

    Wed, 4 Sep 2024 10:17:25 +0300 (now)

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -7,50 +7,8 @@ return {
   event = 'VeryLazy',
   config = function()
     require('which-key').setup {
-      plugins = {
-        marks = true,     -- shows a list of your marks on ' and `
-        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-        -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-        -- No actual key bindings are created
-        spelling = {
-          enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-          suggestions = 20, -- how many suggestions should be shown in the list?
-        },
-        presets = {
-          text_objects = true, -- help for text objects triggered after entering an operator
-          nav = true,          -- misc bindings to work with windows
-          z = true,            -- bindings for folds, spelling and others prefixed with z
-          g = true,            -- bindings for prefixed with g
-        },
-      },
-      -- add operators that will trigger motion and text object completion
-      -- to enable all native operators, set the preset / operators plugin above
-      icons = {
-        breadcrumb = '»', -- symbol used in the command line area that shows your active key combo
-        separator = '➜', -- symbol used between a key and it's label
-        group = '+', -- symbol prepended to a group
-      },
       win = {
-        border = 'rounded',       -- none, single, double, shadow
-        position = 'bottom',      -- bottom, top
-        padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-        zindex = 1000,            -- positive value to position WhichKey above other floating windows.
-      },
-      layout = {
-        height = { min = 4, max = 25 }, -- min and max height of the columns
-        width = { min = 20, max = 50 }, -- min and max width of the columns
-        spacing = 3,                    -- spacing between columns
-        align = 'left',                 -- align columns left, center or right
-      },
-      show_help = true,                 -- show a help message in the command line for using WhichKey
-      show_keys = true,                 -- show the currently pressed key and its label as a message in the command line
-      triggers = { "<leader>" },        -- or specifiy a list manually
-      -- list of triggers, where WhichKey should not wait for timeoutlen and show immediately
-      -- disable the WhichKey popup for certain buf types and file types.
-      -- Disabled by default for Telescope
-      disable = {
-        buftypes = {},
-        filetypes = {},
+        border = 'rounded',
       },
     }
 
