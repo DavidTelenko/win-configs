2ca6ae12695723fda0054502443550b2d8d9a76b
DavidTelenko <davidtelenko11@gmail.com>
2024-09-08 23:46:55 +0300

    Sun, 8 Sep 2024 23:46:54 +0300 (now)

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -13,19 +13,22 @@ return {
         -- Add languages to be installed here that you want installed for treesitter
         -- This seem to completely butcher treesitter parsers installation
         ensure_installed = {
-          -- 'c',
-          -- 'cpp',
-          -- 'go',
-          -- 'lua',
-          -- 'python',
-          -- 'rust',
-          -- 'vimdoc',
-          -- 'vim',
-          -- 'bash',
-          -- 'nu',
-          -- 'regex',
-          -- 'markdown',
-          -- 'markdown_inline',
+          'c',
+          'cpp',
+          'go',
+          'lua',
+          'python',
+          'rust',
+          'vimdoc',
+          'vim',
+          'bash',
+          'nu',
+          'regex',
+          'markdown',
+          'markdown_inline',
+          'typescript',
+          'javascript',
+          'tsx',
         },
         sync_install = true,
         ignore_install = {},
@@ -91,28 +94,28 @@ return {
       }
       require('which-key').add {
         {
-          mode = { "o" },
-          { "", desc = "ia" },
-          { "", desc = "aa" },
-          { "", desc = "ac" },
-          { "", desc = "if" },
-          { "", desc = "ic" },
-          { "", desc = "af" },
+          mode = { 'o' },
+          { '', desc = 'ia' },
+          { '', desc = 'aa' },
+          { '', desc = 'ac' },
+          { '', desc = 'if' },
+          { '', desc = 'ic' },
+          { '', desc = 'af' },
         },
       }
 
       require('treesitter-context').setup {
-        enable = false,           -- Enable this plugin (Can be enabled/disabled later via commands)
-        max_lines = 1,            -- How many lines the window should span. Values <= 0 mean no limit.
-        min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        enable = false, -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 1, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
         line_numbers = true,
         multiline_threshold = 20, -- Maximum number of lines to show for a single context
-        trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
+        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
         -- Separator between context and content. Should be a single character string, like '-'.
         -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
         separator = nil,
-        zindex = 20,     -- The Z-index of the context window
+        zindex = 20, -- The Z-index of the context window
         on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
       }
 
