b4916c33af0037954ce8c29a70b38be268fd9f8e
David Telekno <davidtelenko11@gmail.com>
2024-09-17 18:06:33 +0300

    Tue, 17 Sep 2024 18:06:33 +0300 (now)

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -120,7 +120,7 @@ return {
         jdtls = {},
         rust_analyzer = {},
         biome = {},
-        tsserver = {},
+        ts_ls = {},
         -- gopls = {},
         -- pyright = {},
         -- ols = {},
@@ -153,7 +153,6 @@ return {
       mason_lspconfig.setup_handlers {
         function(server_name)
           if server_name == 'tsserver' then
-            server_name = 'ts_ls'
             require('lspconfig')[server_name].setup {
               capabilities = capabilities,
               on_attach = on_attach,
