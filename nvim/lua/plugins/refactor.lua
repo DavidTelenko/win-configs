1e64e685a1a10aaac754b13144d24c37d8aa4a5a
DavidTelenko <davidtelenko11@gmail.com>
2024-09-29 18:36:01 +0300

    Sun, 29 Sep 2024 18:36:01 +0300 (now)

--- nvim/lua/plugins/refactor.lua
+++ nvim/lua/plugins/refactor.lua
@@ -1,13 +1,13 @@
 return {
   'ThePrimeagen/refactoring.nvim',
   keys = {
-    { '<leader>re',  ':Refactor extract ',              mode = 'x',                desc = '[E]xtract' },
-    { '<leader>rf',  ':Refactor extract_to_file ',      mode = 'x',                desc = 'Extract to [F]ile' },
-    { '<leader>rv',  ':Refactor extract_var ',          mode = 'x',                desc = 'Extract [V]ariable' },
-    { '<leader>rI',  ':Refactor inline_func',           desc = '[I]nline function' },
-    { '<leader>rb',  ':Refactor extract_block',         desc = 'Extract [B]lock' },
+    { '<leader>re', ':Refactor extract ', mode = 'x', desc = '[E]xtract' },
+    { '<leader>rf', ':Refactor extract_to_file ', mode = 'x', desc = 'Extract to [F]ile' },
+    { '<leader>rv', ':Refactor extract_var ', mode = 'x', desc = 'Extract [V]ariable' },
+    { '<leader>rI', ':Refactor inline_func', desc = '[I]nline function' },
+    { '<leader>rb', ':Refactor extract_block', desc = 'Extract [B]lock' },
     { '<leader>rbf', ':Refactor extract_block_to_file', desc = 'To [F]ile' },
-    { '<leader>ri',  ':Refactor inline_var',            mode = { 'n', 'x' },       desc = '[I]nline variable' },
+    { '<leader>ri', ':Refactor inline_var', mode = { 'n', 'x' }, desc = '[I]nline variable' },
   },
   dependencies = {
     'nvim-lua/plenary.nvim',
@@ -16,8 +16,8 @@ return {
   config = function()
     require('refactoring').setup {}
 
-    require('which-key').register({
-      ['<leader>r'] = '[R]efactor',
-    }, { mode = 'x' })
+    require('which-key').add {
+      { '<leader>r', desc = '[R]efactor', mode = 'x' },
+    }
   end,
 }
