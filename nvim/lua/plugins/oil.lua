a192dc75af4876bc082f8d67be1c940f8f80345e
David Telekno <davidtelenko11@gmail.com>
2024-10-31 19:35:21 +0200

    Thu, 31 Oct 2024 19:35:21 +0200 (now)

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -63,14 +63,14 @@ return {
       -- See :help oil-actions for a list of all available actions
       keymaps = {
         ['g?'] = 'actions.show_help',
-        ['<right>'] = 'actions.select',
+        ['l'] = 'actions.select',
         ['<C-s>'] = 'actions.select_vsplit',
         ['<C-h>'] = 'actions.select_split',
         ['<C-t>'] = 'actions.select_tab',
         ['<C-p>'] = 'actions.preview',
         ['<C-c>'] = 'actions.close',
         ['<C-l>'] = 'actions.refresh',
-        ['<left>'] = 'actions.parent',
+        ['h'] = 'actions.parent',
         ['_'] = 'actions.open_cwd',
         ['`'] = 'actions.cd',
         ['~'] = 'actions.tcd',
