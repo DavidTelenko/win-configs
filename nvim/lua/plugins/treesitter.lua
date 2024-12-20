07d4f6e6d69840e4db885e052a1238ee1e36d8ae
DavidTelenko <davidtelenko11@gmail.com>
2024-12-20 17:21:55 +0200

    Fri, 20 Dec 2024 17:21:55 +0200 (now)

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -77,20 +77,44 @@ return {
             enable = true,
             set_jumps = true, -- whether to set jumps in the jumplist
             goto_next_start = {
-              [']m'] = '@function.outer',
-              [']]'] = '@class.outer',
+              [']m'] = {
+                query = '@function.outer',
+                desc = 'Next start of function',
+              },
+              [']]'] = {
+                query = '@class.outer',
+                desc = 'Next start of class',
+              },
             },
             goto_next_end = {
-              [']M'] = '@function.outer',
-              [']['] = '@class.outer',
+              [']M'] = {
+                query = '@function.outer',
+                desc = 'Next end of function',
+              },
+              [']['] = {
+                query = '@class.outer',
+                desc = 'Next end of class',
+              },
             },
             goto_previous_start = {
-              ['[m'] = '@function.outer',
-              ['[['] = '@class.outer',
+              ['[m'] = {
+                query = '@function.outer',
+                desc = 'Previous start of function',
+              },
+              ['[['] = {
+                query = '@class.outer',
+                desc = 'Previous start of class',
+              },
             },
             goto_previous_end = {
-              ['[M'] = '@function.outer',
-              ['[]'] = '@class.outer',
+              ['[M'] = {
+                query = '@function.outer',
+                desc = 'Previous end of function',
+              },
+              ['[]'] = {
+                query = '@class.outer',
+                desc = 'Previous end of class',
+              },
             },
           },
           swap = {
