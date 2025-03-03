adf44b253a4b311bab14d2306faf81225243a95e
David Telenko <63603558+DavidTelenko@users.noreply.github.com>
2025-03-04 00:25:11 +0200

    Tue, 4 Mar 2025 00:25:11 +0200 (now)

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,65 +1,30 @@
--- [[ Basic Keymaps ]]
-
--- Keymaps for better default experience
+-- [[ Keymaps for better default experience ]]
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set({ 'n', 'i', 'v' }, '<F1>', '<Esc>', { silent = true })
 
--- Remap for dealing with word wrap
-vim.keymap.set(
-  'n',
-  '0',
-  "v:count == 0 ? 'g0' : '0'",
-  { expr = true, silent = true }
-)
-vim.keymap.set(
-  'n',
-  '$',
-  "v:count == 0 ? 'g$' : '$'",
-  { expr = true, silent = true }
-)
-vim.keymap.set(
-  'n',
-  'k',
-  "v:count == 0 ? 'gk' : 'k'",
-  { expr = true, silent = true }
-)
-vim.keymap.set(
-  'n',
-  'j',
-  "v:count == 0 ? 'gj' : 'j'",
-  { expr = true, silent = true }
-)
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+vim.keymap.set('n', 'J', 'mzJ`z')
 
--- Diagnostic keymaps
-vim.keymap.set(
-  'n',
-  '[d',
-  vim.diagnostic.goto_prev,
-  { desc = 'Go to previous diagnostic message' }
-)
-vim.keymap.set(
-  'n',
-  ']d',
-  vim.diagnostic.goto_next,
-  { desc = 'Go to next diagnostic message' }
-)
+-- Useless for me - L and H
+vim.keymap.set({ 'n', 'v' }, 'L', '$')
+vim.keymap.set({ 'n', 'v' }, 'H', '_')
+
+-- Jump to begin end of a tag
+vim.keymap.set('n', ']t', 'vat<esc>', { desc = 'Jump to end of a tag' })
+vim.keymap.set('n', '[t', 'vato<esc>', { desc = 'Jump to begining of a tag' })
+
+-- disable default control keymaps
+vim.keymap.set('i', '<C-k>', '<NOP>', {})
+vim.keymap.set('i', '<C-j>', '<NOP>', {})
 
 vim.keymap.set(
-  'n',
-  '<leader>e',
-  vim.diagnostic.open_float,
-  { desc = 'Open floating diagnostic message' }
+  'v',
+  'p',
+  'P',
+  { desc = 'Paste without copying selected text in visual mode' }
 )
-vim.keymap.set(
-  'n',
-  '<leader>q',
-  vim.diagnostic.setloclist,
-  { desc = 'Open diagnostics list' }
-)
-
-vim.keymap.set('n', '<leader>i', function()
-  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {})
-end, { desc = 'Enable [I]nline hints' })
+-- vim.keymap.set('v', 'p', '"0p')
 
 vim.keymap.set(
   'n',
@@ -87,6 +52,33 @@ vim.keymap.set(
   { desc = 'A bit more convenient save file' }
 )
 
+-- Remaps for dealing with word wrap
+vim.keymap.set(
+  'n',
+  '0',
+  "v:count == 0 ? 'g0' : '0'",
+  { expr = true, silent = true }
+)
+vim.keymap.set(
+  'n',
+  '$',
+  "v:count == 0 ? 'g$' : '$'",
+  { expr = true, silent = true }
+)
+vim.keymap.set(
+  'n',
+  'k',
+  "v:count == 0 ? 'gk' : 'k'",
+  { expr = true, silent = true }
+)
+vim.keymap.set(
+  'n',
+  'j',
+  "v:count == 0 ? 'gj' : 'j'",
+  { expr = true, silent = true }
+)
+
+-- Navigation in insert mode
 vim.keymap.set('i', '<A-k>', '<up>', { desc = 'Navigation up in insert mode' })
 vim.keymap.set(
   'i',
@@ -132,9 +124,35 @@ vim.keymap.set(
   { desc = 'Ctrl + Del "default" behavior' }
 )
 
--- paste without copying selected text in visual mode
-vim.keymap.set('v', 'p', 'P')
--- vim.keymap.set('v', 'p', '"0p')
+-- [[ Diagnostic keymaps ]]
+vim.keymap.set(
+  'n',
+  '[d',
+  vim.diagnostic.goto_prev,
+  { desc = 'Go to previous diagnostic message' }
+)
+vim.keymap.set(
+  'n',
+  ']d',
+  vim.diagnostic.goto_next,
+  { desc = 'Go to next diagnostic message' }
+)
+vim.keymap.set(
+  'n',
+  '<leader>e',
+  vim.diagnostic.open_float,
+  { desc = 'Open floating diagnostic message' }
+)
+vim.keymap.set(
+  'n',
+  '<leader>q',
+  vim.diagnostic.setloclist,
+  { desc = 'Open diagnostics list' }
+)
+
+vim.keymap.set('n', '<leader>i', function()
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {})
+end, { desc = 'Enable [I]nline hints' })
 
 -- How do i exit terminal in vim?
 vim.keymap.set('t', '<esc>', '<C-\\><C-n>', { desc = 'Exit terminal' })
@@ -145,18 +163,6 @@ vim.keymap.set(
   { desc = 'Ctrl + Backspace "default" behavior' }
 )
 vim.keymap.set('n', '<leader>T', vim.cmd.terminal, { desc = 'Open [T]erminal' })
-
--- Useless for me - L and H
-vim.keymap.set({ 'n', 'v' }, 'L', 'e')
-vim.keymap.set({ 'n', 'v' }, 'H', 'b')
-
--- Jump to begin end of a tag
-vim.keymap.set('n', ']t', 'vat<esc>', { desc = 'Jump to end of a tag' })
-vim.keymap.set('n', '[t', 'vato<esc>', { desc = 'Jump to begining of a tag' })
-
--- disable default control keymaps
-vim.keymap.set('i', '<C-k>', '<NOP>', {})
-vim.keymap.set('i', '<C-j>', '<NOP>', {})
 
 -- lua execute
 vim.keymap.set(
@@ -202,6 +208,12 @@ vim.keymap.set(
   '<leader>qc',
   '<cmd>cclose<CR>',
   { desc = '[C]lose quicklist' }
+)
+vim.keymap.set(
+  'n',
+  '<leader>cr',
+  '<cmd>LspRestart<cr>',
+  { desc = '[R]estart Lsp Server' }
 )
 
 -- vim: ts=2 sts=2 sw=2 et
