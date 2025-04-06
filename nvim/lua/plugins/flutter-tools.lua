f8891a0b4331c2b0139791ee30210c087827765d
David Telenko <63603558+DavidTelenko@users.noreply.github.com>
2025-04-06 20:45:53 +0300

    Sun, 6 Apr 2025 20:45:53 +0300 (now)

--- nvim/lua/plugins/flutter-tools.lua
+++ nvim/lua/plugins/flutter-tools.lua
@@ -1,7 +1,7 @@
 return {
   'akinsho/flutter-tools.nvim',
   enabled = true,
-  keys = { '<leader>ft', '<leader>fr' },
+  ft = { 'dart' },
   dependencies = {
     'nvim-lua/plenary.nvim',
     'stevearc/dressing.nvim',
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -9,27 +9,43 @@ return {
     -- document existing key chains
     require('which-key').add {
       { '<leader>c', group = 'Code', mode = { 'x', 'n' } },
-      { '<leader>c_', hidden = true },
       { '<leader>d', group = 'Debug' },
-      { '<leader>d_', hidden = true },
       { '<leader>f', group = 'File' },
-      { '<leader>f_', hidden = true },
       { '<leader>g', group = 'Git', mode = { 'x', 'n' } },
-      { '<leader>g_', hidden = true },
       { '<leader>r', group = 'Refactor', mode = { 'x', 'n' } },
-      { '<leader>r_', hidden = true },
       { '<leader>s', group = 'Search' },
-      { '<leader>s_', hidden = true },
-      { '<leader>w', group = 'Workspace' },
-      { '<leader>w_', hidden = true },
-      { '<leader>x', group = 'Trouble' },
-      { '<leader>x_', hidden = true },
       { '<leader>t', group = 'Test' },
-      { '<leader>t_', hidden = true },
-      { '<leader>h', group = 'Harpoon' },
-      { '<leader>h_', hidden = true },
-      { '<leader>i', group = 'Iron' },
-      { '<leader>i_', hidden = true },
+      { '<leader>x', group = 'Trouble' },
+      {
+        '<leader>w',
+        group = 'Workspace',
+        icon = { icon = '', color = 'green' },
+      },
+      {
+        '<leader>h',
+        group = 'Quick file (Harpoon)',
+        icon = { icon = '󰓾', color = 'green' },
+      },
+      {
+        '<leader>o',
+        group = 'Open parent directory (Oil)',
+        icon = { icon = '', color = 'yellow' },
+      },
+      {
+        '<leader>z',
+        group = 'Zen mode',
+        icon = { icon = '', color = 'orange' },
+      },
+      {
+        '<leader>i',
+        group = 'Repl (Iron)',
+        icon = { icon = '', color = 'blue' },
+      },
+      {
+        '<leader>p',
+        group = 'Pick color',
+        icon = { icon = '', color = 'blue' },
+      },
     }
   end,
 }
