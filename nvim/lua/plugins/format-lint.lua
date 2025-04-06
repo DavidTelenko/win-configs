b262103eb52e7890e36334ff02d8dac58bff2ec1
David Telenko <63603558+DavidTelenko@users.noreply.github.com>
2025-04-07 00:37:48 +0300

    Mon, 7 Apr 2025 00:37:48 +0300 (now)

--- nvim/lua/plugins/format-lint.lua
+++ nvim/lua/plugins/format-lint.lua
@@ -11,16 +11,6 @@ return {
   config = function()
     local conform = require 'conform'
 
-    --- NOTE: Here's the logic for you if you'll ever forget:
-    ---
-    --- !The rule of thumb: first ones listed using `first` are exotics which
-    --- are not installed using Mason (the global "defaults"), latter ones is
-    --- more and more global, the last one is preferably the one you've
-    --- installed with mason
-    ---
-    --- !Also: if you install something with Mason it WILL BE FOUND anyway,
-    --- because Mason adds the tool to the nvim Path so take this into
-    --- consideration.
     local function first(bufnr, formatters)
       for _, formatter in ipairs(formatters) do
         if conform.get_formatter_info(formatter, bufnr).available then
@@ -36,19 +26,61 @@ return {
       end
     end
 
-    local js_ts_formatters = function(bufnr)
-      local formatter = first(bufnr, { 'biome', 'deno_fmt', 'prettierd' })
-      if formatter == 'prettierd' then
-        return { formatter, 'eslint_d' }
+    --- TODO: starting from this point make this configuration modular
+    local eslint_configs = {
+      'eslint.config.js',
+      'eslint.config.mjs',
+      'eslint.config.cjs',
+      'eslint.config.ts',
+      'eslint.config.mts',
+      'eslint.config.cts',
+    }
+    local biome_configs = { 'biome.json', 'biome.jsonc' }
+
+    local linters_configs = {
+      biomejs = biome_configs,
+      eslint_d = eslint_configs,
+    }
+
+    local formatter_configs = {
+      biome = biome_configs,
+      eslint_d = eslint_configs,
+      prettierd = {
+        '.prettierrc',
+        '.prettierrc.json',
+        '.prettierrc.js',
+        'prettier.config.js',
+        'prettier.config.mjs',
+        'prettier.config.cjs',
+      },
+      deno = { 'deno.json', 'deno.jsonc' },
+    }
+
+    local function from_local_configs(config_files)
+      local dir = vim.fn.getcwd()
+      local result = {}
+      for formatter, patterns in pairs(config_files) do
+        for _, pattern in ipairs(patterns) do
+          local config_file = dir .. '/' .. pattern
+          if vim.fn.filereadable(config_file) == 1 then
+            table.insert(result, formatter)
+          end
+        end
       end
-      return { formatter }
+      return result
+    end
+
+    local js_ts_formatters = function(bufnr)
+      return from_local_configs(formatter_configs)
     end
 
     require('mason-tool-installer').setup {
       ensure_installed = {
+        'biome',
         'eslint_d',
         'prettierd',
         'stylua',
+        'yq',
       },
     }
 
@@ -78,13 +110,7 @@ return {
     }
 
     local lint = require 'lint'
-    local formatter_to_linter = {
-      biome = 'biomejs',
-      eslint_d = 'eslint_d',
-    }
-
-    local formatter = first(0, { 'biome', 'eslint_d' })
-    local js_ts_linters = { formatter_to_linter[formatter] }
+    local js_ts_linters = from_local_configs(linters_configs)
 
     lint.linters_by_ft = {
       cpp = { 'cpplint' },
@@ -100,7 +126,7 @@ return {
     vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
       callback = function(args)
         local buftype =
-            vim.api.nvim_get_option_value('buftype', { buf = args.buf })
+          vim.api.nvim_get_option_value('buftype', { buf = args.buf })
 
         if buftype == '' then
           lint.try_lint()
