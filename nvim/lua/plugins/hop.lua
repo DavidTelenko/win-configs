8d39dc0cae126a172c3e501a3194e76c6e876fbd
David Telenko <63603558+DavidTelenko@users.noreply.github.com>
2025-04-07 11:17:36 +0300

    Mon, 7 Apr 2025 11:17:36 +0300 (now)

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -10,18 +10,17 @@ return {
       desc = 'Hop anywhere',
     },
     {
-      'sf',
-      '<cmd>HopChar1<cr>',
-      mode = { 'n', 'v' },
-      desc = 'Hop to character',
-    },
-    {
       'ss',
       '<cmd>HopPattern<cr>',
       mode = { 'n', 'v' },
       desc = 'Hop to pattern',
     },
-    { 'sl', '<cmd>HopLine<cr>', mode = { 'n', 'v' }, desc = 'Hop to line' },
+    {
+      'sl',
+      '<cmd>HopLine<cr>',
+      mode = { 'n', 'v' },
+      desc = 'Hop to line',
+    },
     {
       'st',
       '<cmd>HopNodes<cr>',
@@ -30,7 +29,42 @@ return {
     },
   },
   version = '*',
-  opts = {
-    keys = 'asdfghjkl;',
-  },
+  config = function()
+    local hop = require 'hop'
+    local directions = require('hop.hint').HintDirection
+
+    hop.setup {
+      keys = 'asdfghjkl;',
+    }
+
+    vim.keymap.set('', 'f', function()
+      hop.hint_char1 {
+        direction = directions.AFTER_CURSOR,
+        current_line_only = true,
+      }
+    end, { remap = true })
+
+    vim.keymap.set('', 'F', function()
+      hop.hint_char1 {
+        direction = directions.BEFORE_CURSOR,
+        current_line_only = true,
+      }
+    end, { remap = true })
+
+    vim.keymap.set('', 't', function()
+      hop.hint_char1 {
+        direction = directions.AFTER_CURSOR,
+        current_line_only = true,
+        hint_offset = -1,
+      }
+    end, { remap = true })
+
+    vim.keymap.set('', 'T', function()
+      hop.hint_char1 {
+        direction = directions.BEFORE_CURSOR,
+        current_line_only = true,
+        hint_offset = 1,
+      }
+    end, { remap = true })
+  end,
 }
