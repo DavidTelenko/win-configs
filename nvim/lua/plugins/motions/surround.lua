054f625d5dea2c127bbba2ea3c9ff4175f6c72cd
David Telenko <63603558+DavidTelenko@users.noreply.github.com>
2025-04-11 23:23:06 +0300

    Fri, 11 Apr 2025 23:23:06 +0300 (now)

--- nvim/lua/plugins/motions/surround.lua
+++ nvim/lua/plugins/motions/surround.lua
@@ -1,5 +1,87 @@
 return {
   'kylechui/nvim-surround',
-  keys = { { 'ds' }, { 'cs' }, { 'S', mode = 'v' } },
-  opts = {},
+  keys = {
+    { 'ds' },
+    { 'cs' },
+    { 'ys' },
+    { 's', mode = 'v' },
+  },
+  opts = {
+    keymaps = {
+      visual = 's',
+    },
+  },
+  config = function(_, opts)
+    local surround = require 'nvim-surround'
+    local surr_utils = require 'nvim-surround.config'
+    local ts_utils = require 'nvim-treesitter.ts_utils'
+
+    surround.setup(opts)
+
+    surround.buffer_setup {
+      surrounds = {
+        g = {
+          add = function()
+            local result = surr_utils.get_input 'Enter the generic name: '
+            if result then
+              return { { result .. '<' }, { '>' } }
+            end
+          end,
+          find = function()
+            return surr_utils.get_selection { node = 'generic_type' }
+          end,
+          delete = '^(.-<)().-(>)()$',
+          change = {
+            target = '^(.-<)().-(>)()$',
+            replacement = function()
+              local result = surr_utils.get_input 'Enter the generic name: '
+              if result then
+                return { { result .. '<' }, { '>' } }
+              end
+            end,
+          },
+        },
+        s = {
+          add = function()
+            local cur = ts_utils.get_node_at_cursor(0, true)
+            local language = vim.bo.filetype
+            local is_js_ts = (
+              language == 'javascript'
+              or language == 'javascriptreact'
+              or language == 'typescript'
+              or language == 'typescriptreact'
+            )
+
+            if is_js_ts and cur ~= nil then
+              local cur_type = cur:type()
+              local interpolation_surround = { { '${' }, { '}' } }
+              if
+                cur and (cur_type == 'string' or cur_type == 'string_fragment')
+              then
+                vim.cmd.normal 'csq`'
+                return interpolation_surround
+              elseif cur and cur_type == 'template_string' then
+                return interpolation_surround
+              else
+                return { { '`${' }, { '}`' } }
+              end
+            end
+          end,
+        },
+        l = {
+          add = function()
+            local input = surr_utils.get_input 'Link name: '
+            if input then
+              return {
+                { '[' .. input .. '](' },
+                { ')' },
+              }
+            end
+          end,
+          find = '%b[]%b()',
+          delete = '^(%[)().-(%]%b())()$',
+        },
+      },
+    }
+  end,
 }
--- nvim/lua/plugins/motions/textobj.lua
+++ nvim/lua/plugins/motions/textobj.lua
@@ -1,17 +1,11 @@
 return {
   'chrisgrieser/nvim-various-textobjs',
+  keys = { 'v', 'd', 'y' },
   opts = {
-    keymaps = { useDefaults = false },
-  },
-  keys = {
-    { 'is', desc = 'subword', mode = { 'o', 'x' } },
-    { 'as', desc = 'subword', mode = { 'o', 'x' } },
-    { 'ib', desc = 'any bracket', mode = { 'o', 'x' } },
-    { 'ab', desc = 'any bracket', mode = { 'o', 'x' } },
-    { 'iq', desc = 'any quoute', mode = { 'o', 'x' } },
-    { 'aq', desc = 'any quoute', mode = { 'o', 'x' } },
-    { 'ii', desc = 'indentation', mode = { 'o', 'x' } },
-    { 'ai', desc = 'indentation', mode = { 'o', 'x' } },
+    keymaps = {
+      useDefaults = true,
+      disabledDefaults = { 'iS', 'aS', 'L' },
+    },
   },
   config = function(_, opts)
     local objs = require 'various-textobjs'
@@ -24,29 +18,5 @@ return {
     vim.keymap.set({ 'o', 'x' }, 'as', function()
       objs.subword 'outer'
     end, { remap = true, desc = 'subword' })
-
-    vim.keymap.set({ 'o', 'x' }, 'ib', function()
-      objs.anyBracket 'inner'
-    end, { remap = true, desc = 'any bracket' })
-
-    vim.keymap.set({ 'o', 'x' }, 'ab', function()
-      objs.anyBracket 'outer'
-    end, { remap = true, desc = 'any bracket' })
-
-    vim.keymap.set({ 'o', 'x' }, 'iq', function()
-      objs.anyQuote 'inner'
-    end, { remap = true, desc = 'any quoute' })
-
-    vim.keymap.set({ 'o', 'x' }, 'aq', function()
-      objs.anyQuote 'outer'
-    end, { remap = true, desc = 'any quoute' })
-
-    vim.keymap.set({ 'o', 'x' }, 'ii', function()
-      objs.indentation 'inner'
-    end, { remap = true, desc = 'indentation' })
-
-    vim.keymap.set({ 'o', 'x' }, 'ai', function()
-      objs.indentation 'outer'
-    end, { remap = true, desc = 'indentation' })
   end,
 }
