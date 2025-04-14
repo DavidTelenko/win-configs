978b34b2acf2a807c9ae93e1d06a828a728ee2a9
David Telenko <63603558+DavidTelenko@users.noreply.github.com>
2025-04-14 18:22:27 +0300

    Mon, 14 Apr 2025 18:22:26 +0300 (now)

--- nvim/lua/plugins/core/lualine.lua
+++ nvim/lua/plugins/core/lualine.lua
@@ -1,117 +1,113 @@
 return {
   'nvim-lualine/lualine.nvim',
   lazy = false,
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = 'auto',
-      component_separators = '',
-      section_separators = { right = '', left = '' },
-      disabled_filetypes = {
-        statusline = {},
-        winbar = {},
+  opts = function()
+    local my_filename = require('lualine.components.filename'):extend()
+    my_filename.apply_icon = require('lualine.components.filetype').apply_icon
+
+    return {
+      options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = '',
+        section_separators = { right = '', left = '' },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = true,
+        refresh = {
+          statusline = 100,
+          tabline = 1000,
+          winbar = 1000,
+        },
       },
-      ignore_focus = {},
-      always_divide_middle = true,
-      globalstatus = true,
-      refresh = {
-        statusline = 100,
-        tabline = 1000,
-        winbar = 1000,
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+          { 'branch', icon = '󰘬' },
+          'diagnostics',
+        },
+        lualine_c = {
+          my_filename,
+        },
+        lualine_x = {
+          {
+            'lsp_status',
+            icon = '',
+            symbols = {
+              done = '',
+              separator = ' ',
+            },
+            ignore_lsp = {
+              'emmet_language_server',
+              'tailwindcss',
+            },
+          },
+        },
+        lualine_y = {
+          {
+            'diff',
+            symbols = { added = ' ', modified = ' ', removed = ' ' },
+          },
+        },
+        lualine_z = { 'location' },
       },
-    },
-    sections = {
-      lualine_a = { 'mode' },
-      lualine_b = {
-        { 'branch', icon = '󰘬' },
-        'diagnostics',
-      },
-      lualine_c = {
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {
+        'lazy',
+        'mason',
+        'quickfix',
         {
-          'filetype',
-          icon_only = true,
-          colored = false,
-          padding = { left = 1 },
+          sections = {
+            lualine_a = {
+              function()
+                return '󰘬 ' .. vim.fn.FugitiveHead()
+              end,
+            },
+            lualine_z = { 'location' },
+          },
+          filetypes = { 'fugitive' },
         },
         {
-          'filename',
-          padding = { right = 1 },
+          sections = {
+            lualine_a = {
+              function()
+                local ft = vim.opt_local.filetype:get()
+                return (ft == 'undotree') and '󰐅 Undotree'
+                  or (ft == 'diff') and '󰐆 Diff'
+              end,
+            },
+            lualine_z = { 'location' },
+          },
+          filetypes = { 'undotree', 'diff' },
         },
-      },
-      lualine_x = {
         {
-          'lsp_status',
-          icon = '',
-          symbols = {
-            done = '',
-            separator = ' ',
+          sections = {
+            lualine_a = {
+              function()
+                return ' Telescope'
+              end,
+            },
           },
-          ignore_lsp = {
-            'emmet_language_server',
-            'tailwindcss',
-          },
+          filetypes = { 'TelescopePrompt' },
         },
-      },
-      lualine_y = {
         {
-          'diff',
-          symbols = { added = ' ', modified = ' ', removed = ' ' },
-        },
-      },
-      lualine_z = { 'location' },
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {
-      'lazy',
-      'mason',
-      'quickfix',
-      {
-        sections = {
-          lualine_a = {
-            function()
-              return '󰘬 ' .. vim.fn.FugitiveHead()
-            end,
+          sections = {
+            lualine_a = {
+              function()
+                return ' Harpoon'
+              end,
+            },
+            lualine_z = { 'location' },
           },
-          lualine_z = { 'location' },
+          filetypes = { 'harpoon' },
         },
-        filetypes = { 'fugitive' },
       },
-      {
-        sections = {
-          lualine_a = {
-            function()
-              local ft = vim.opt_local.filetype:get()
-              return (ft == 'undotree') and '󰐅 Undotree'
-                or (ft == 'diff') and '󰐆 Diff'
-            end,
-          },
-          lualine_z = { 'location' },
-        },
-        filetypes = { 'undotree', 'diff' },
-      },
-      {
-        sections = {
-          lualine_a = {
-            function()
-              return ' Telescope'
-            end,
-          },
-        },
-        filetypes = { 'TelescopePrompt' },
-      },
-      {
-        sections = {
-          lualine_a = {
-            function()
-              return ' Harpoon'
-            end,
-          },
-          lualine_z = { 'location' },
-        },
-        filetypes = { 'harpoon' },
-      },
-    },
-  },
+    }
+  end,
 }
