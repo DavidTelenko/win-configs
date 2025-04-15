d06c8f3017ca7e8e7c99e82e37dea54e5487a6b6
David Telenko <63603558+DavidTelenko@users.noreply.github.com>
2025-04-16 01:14:00 +0300

    Wed, 16 Apr 2025 01:14:00 +0300 (now)

--- nvim/lua/plugins/core/debug.lua
+++ nvim/lua/plugins/core/debug.lua
@@ -9,6 +9,7 @@ return {
       { '<leader>du', desc = 'Open Ui' },
       { '<leader>dB', desc = 'Set Breakpoint' },
       { '<leader>db', desc = 'Toggle Breakpoint' },
+      { '<leader>b', desc = 'Toggle Breakpoint' },
       { '<leader>dc', desc = 'Start/Continue' },
       { '<leader>dO', desc = 'Step Out' },
       { '<leader>do', desc = 'Step Over' },
@@ -19,13 +20,9 @@ return {
       local dapui = require 'dapui'
 
       require('mason-nvim-dap').setup {
-        -- Makes a best effort to setup the various debuggers with
-        -- reasonable debug configurations
         automatic_setup = true,
         automatic_installation = true,
 
-        -- You can provide additional configuration to the handlers,
-        -- see mason-nvim-dap README for more information
         handlers = {
           function(config)
             if vim.fn.has 'win32' == 1 then
@@ -69,8 +66,6 @@ return {
           end,
         },
 
-        -- You'll need to check that you have the required things installed
-        -- online, please don't ask me how to install them :)
         ensure_installed = {
           -- Update this to ensure that you have the debuggers for the langs you want
           -- 'cppdbg',
@@ -99,69 +94,69 @@ return {
         desc = 'Toggle Breakpoint',
       })
 
+      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, {
+        desc = 'Toggle Breakpoint',
+      })
+
       vim.keymap.set('n', '<leader>dB', function()
         dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
       end, { desc = 'Set Breakpoint' })
 
-      -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+      -- Toggle to see last session result.
       vim.keymap.set('n', '<leader>du', dapui.toggle, {
         desc = 'Open Ui',
       })
 
       -- Dap UI setup
-      -- For more information, see |:help nvim-dap-ui|
       dapui.setup()
       require('nvim-dap-virtual-text').setup {}
 
       vim.api.nvim_set_hl(0, 'DapBreakpoint', {
         ctermbg = 0,
-        fg = '#fb4934',
         bg = '#261c1c',
       })
 
       vim.api.nvim_set_hl(0, 'DapLogPoint', {
         ctermbg = 0,
-        fg = '#83a598',
         bg = '#261c1c',
       })
 
       vim.api.nvim_set_hl(0, 'DapStopped', {
         ctermbg = 0,
-        fg = '#fb4934',
         bg = '#261c1c',
       })
 
       vim.fn.sign_define('DapBreakpoint', {
         text = '',
-        texthl = 'DapBreakpoint',
+        texthl = 'WarningMsg',
         linehl = 'DapBreakpoint',
         numhl = 'DapBreakpoint',
       })
 
       vim.fn.sign_define('DapBreakpointCondition', {
         text = '',
-        texthl = 'DapBreakpoint',
+        texthl = 'Question',
         linehl = 'DapBreakpoint',
         numhl = 'DapBreakpoint',
       })
 
       vim.fn.sign_define('DapBreakpointRejected', {
         text = '',
-        texthl = 'DapBreakpoint',
+        texthl = 'WarningMsg',
         linehl = 'DapBreakpoint',
         numhl = 'DapBreakpoint',
       })
 
       vim.fn.sign_define('DapLogPoint', {
         text = '',
-        texthl = 'DapLogPoint',
+        texthl = 'Question',
         linehl = 'DapLogPoint',
         numhl = 'DapLogPoint',
       })
 
       vim.fn.sign_define('DapStopped', {
         text = '',
-        texthl = 'DapStopped',
+        texthl = 'WarningMsg',
         linehl = 'DapStopped',
         numhl = 'DapStopped',
       })
