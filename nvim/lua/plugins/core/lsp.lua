edefa6d33d9004436385640c17204ba88e9e653d
David Telenko <63603558+DavidTelenko@users.noreply.github.com>
2025-04-16 21:30:57 +0300

    Wed, 16 Apr 2025 21:30:56 +0300 (now)

--- nvim/lua/plugins/core/lsp.lua
+++ nvim/lua/plugins/core/lsp.lua
@@ -191,7 +191,19 @@ return {
         rust_analyzer = {},
         svelte = {},
         tailwindcss = {},
-        ts_ls = {},
+        ts_ls = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = 'all',
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+        },
         -- vtsls = {},
         jsonls = {
           json = {
