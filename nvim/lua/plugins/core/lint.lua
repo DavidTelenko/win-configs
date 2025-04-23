d4ed167f0e1c75d1e1a792efd596ea7ff0d638f0
David Telenko <63603558+DavidTelenko@users.noreply.github.com>
2025-04-23 15:12:05 +0300

    feat: add dirchanged event handling for conform and lint plugins

--- nvim/lua/plugins/core/lint.lua
+++ nvim/lua/plugins/core/lint.lua
@@ -1,25 +1,29 @@
 return {
   'mfussenegger/nvim-lint',
-  event = { 'BufWritePost' },
+  event = { 'BufWritePost', 'DirChanged' },
   config = function()
     local lint = require 'lint'
     local h = require 'helpers.general'
-    local js_ts_linters = h.first(h.require_config { 'eslint_d', 'biomejs' })
-    local configs = require 'helpers.configs'
 
-    for _, name in ipairs(js_ts_linters) do
-      lint.linters[name].cwd = vim.fs.root(vim.fn.getcwd(), configs[name])
+    function setup()
+      local js_ts_linters = h.first(h.require_config { 'eslint_d', 'biomejs' })
+      local configs = require 'helpers.configs'
+
+      for _, name in ipairs(js_ts_linters) do
+        lint.linters[name].cwd = vim.fs.root(vim.fn.getcwd(), configs[name])
+      end
+
+      lint.linters_by_ft = {
+        cpp = { 'cpplint' },
+        javascript = js_ts_linters,
+        javascriptreact = js_ts_linters,
+        python = { 'pylint' },
+        svelte = js_ts_linters,
+        typescript = js_ts_linters,
+        typescriptreact = js_ts_linters,
+      }
     end
-
-    lint.linters_by_ft = {
-      cpp = { 'cpplint' },
-      javascript = js_ts_linters,
-      javascriptreact = js_ts_linters,
-      python = { 'pylint' },
-      svelte = js_ts_linters,
-      typescript = js_ts_linters,
-      typescriptreact = js_ts_linters,
-    }
+    setup()
 
     -- Lint auto command
     vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
@@ -32,6 +36,13 @@ return {
           lint.try_lint 'cspell'
         end
       end,
+    })
+
+    vim.api.nvim_create_autocmd('DirChanged', {
+      group = vim.api.nvim_create_augroup('LintDirChanged', {
+        clear = true,
+      }),
+      callback = setup,
     })
 
     --- TODO: find a better way to offload some sections in lualine
