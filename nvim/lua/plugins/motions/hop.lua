9b44e69e9fed5e022e87a80a1eb395305b4b241f
David Telenko <63603558+DavidTelenko@users.noreply.github.com>
2025-06-02 19:24:13 +0300

    feat(hop): remove f F t T mappings instead fallback to default and use dedicated

--- nvim/lua/plugins/motions/hop.lua
+++ nvim/lua/plugins/motions/hop.lua
@@ -20,48 +20,17 @@ return {
       mode = { 'n', 'v' },
       desc = 'Hop to treesitter nodes',
     },
-    { 'f', desc = 'Hop to character', mode = { 'n', 'v' } },
-    { 'F', desc = 'Hop back to character', mode = { 'n', 'v' } },
-    { 't', desc = 'Hop up to character', mode = { 'n', 'v' } },
-    { 'T', desc = 'Hop back up to character', mode = { 'n', 'v' } },
+    { 'sf', desc = 'Hop to character', mode = { 'n', 'v' } },
   },
   opts = {},
   config = function(_, opts)
     local hop = require 'hop'
-    local directions = require('hop.hint').HintDirection
 
     hop.setup(opts)
 
-    vim.keymap.set({ 'n', 'v' }, 'f', function()
+    vim.keymap.set({ 'n', 'v' }, 'sf', function()
       hop.hint_char1 {
-        direction = directions.AFTER_CURSOR,
         current_line_only = false,
-        jump_on_sole_occurrence = false,
-      }
-    end, { remap = true })
-
-    vim.keymap.set({ 'n', 'v' }, 'F', function()
-      hop.hint_char1 {
-        direction = directions.BEFORE_CURSOR,
-        current_line_only = false,
-        jump_on_sole_occurrence = false,
-      }
-    end, { remap = true })
-
-    vim.keymap.set({ 'n', 'v' }, 't', function()
-      hop.hint_char1 {
-        direction = directions.AFTER_CURSOR,
-        current_line_only = false,
-        hint_offset = -1,
-        jump_on_sole_occurrence = false,
-      }
-    end, { remap = true })
-
-    vim.keymap.set({ 'n', 'v' }, 'T', function()
-      hop.hint_char1 {
-        direction = directions.BEFORE_CURSOR,
-        current_line_only = false,
-        hint_offset = 1,
         jump_on_sole_occurrence = false,
       }
     end, { remap = true })
